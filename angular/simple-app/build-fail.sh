d43da8db363dfd8dc1c12efe89053781d1e08e9e
Andres Sainz de Aja <andres.sainz@gmail.com>
2013-09-27 12:41:36 -0300

    Andres - removing commit of build.log for now

--- angular/simple-app/build-fail.sh
+++ angular/simple-app/build-fail.sh
@@ -1,5 +1,1 @@
 echo build number $BUILD_NUMBER failed >> build.log
-cd ../../
-git add angular/simple-app/build.log
-git commit -m "BUILD FAILED - updateing build.log"
-git push origin master
--- angular/simple-app/build-success.sh
+++ angular/simple-app/build-success.sh
@@ -1,5 +1,1 @@
 echo build number $BUILD_NUMBER succedded >> build.log
-cd ../../
-git add angular/simple-app/build.log
-git commit -m "BUILD SUCCEDDED - updateing build.log"
-git push origin master
