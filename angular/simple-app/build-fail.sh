acf569eda55a91083f90879cea1b5d63ba5bf2fe
Andres Sainz de Aja <andres.sainz@gmail.com>
2013-09-27 12:40:35 -0300

    Andres - push changes in build.log

--- angular/simple-app/build-fail.sh
+++ angular/simple-app/build-fail.sh
@@ -2,3 +2,4 @@ echo build number $BUILD_NUMBER failed >> build.log
 cd ../../
 git add angular/simple-app/build.log
 git commit -m "BUILD FAILED - updateing build.log"
+git push origin master
--- angular/simple-app/build-success.sh
+++ angular/simple-app/build-success.sh
@@ -2,3 +2,4 @@ echo build number $BUILD_NUMBER succedded >> build.log
 cd ../../
 git add angular/simple-app/build.log
 git commit -m "BUILD SUCCEDDED - updateing build.log"
+git push origin master
