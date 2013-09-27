ef5217379f459aedb1e52fa191c6f2a12ffe2bab
Andres Sainz de Aja <andres.sainz@gmail.com>
2013-09-27 12:36:29 -0300

    Andres - fix typo

--- angular/simple-app/build-fail.sh
+++ angular/simple-app/build-fail.sh
@@ -1,4 +1,4 @@
 echo build number $BUILD_NUMBER failed >> build.log
 cd ../../
 git add build.log
-git c -m "BUILD FAILED - updateing build.log"
+git commit -m "BUILD FAILED - updateing build.log"
--- angular/simple-app/build-success.sh
+++ angular/simple-app/build-success.sh
@@ -1,4 +1,4 @@
 echo build number $BUILD_NUMBER succedded >> build.log
 cd ../../
 git add build.log
-git c -m "BUILD SUCCEDDED - updateing build.log"
+git commit -m "BUILD SUCCEDDED - updateing build.log"
